0a815a9c1f648b7b7db897c990add60c1ba0f7c6
NickolasDzR <nikolyae2019@gmail.com>
2021-08-07 13:51:18 +0300

    Обновил webpack.mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,34 +1,61 @@
-let mix = require('laravel-mix');
-mix.pug = require('laravel-mix-pug');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+let mix = require("laravel-mix");
+mix.pug = require("laravel-mix-pug");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
+const path = require("path");
+const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
 
-mix.js('src/js/index.js', 'js')
-    .sass('src/styles/vendor/main.scss', 'css')
-    .pug('src/views/index.pug', '../../dist').setPublicPath("dist")
+Config.svgSprite = {
+    loaderOptions: {
+        extract: true
+    },
+    pluginOptions: {
+        plainSprite: true
+    }
+};
+
+mix.alias({
+    "%modules%": path.join(__dirname, "src/blocks/modules"),
+    "%components%": path.join(__dirname, "src/blocks/components")
+})
+    .js("src/js/index.js", "js")
+    .extract()
+    .sass("src/styles/vendor/main.scss", "css/main.css")
     .options({
-        postCss: [require("tailwindcss")]
+        postCss: [require("css-mqpacker"), require("tailwindcss")]
     })
+    .pug("src/views/index.pug", "../../dist")
+    .setPublicPath("dist")
     .webpackConfig({
-        plugins: [
-            new CopyWebpackPlugin({
-                patterns: [
-                    { from: 'src/img/', to: '../dist/img/' },
-                    { from: 'src/fonts/', to: '../dist/fonts' }
-                ],
-            }),
-            new ImageminWebpWebpackPlugin({
+            plugins: [
+                new CopyWebpackPlugin({
+                    patterns: [
+                        {from: "src/img/", to: "../dist/img/"},
+                        {from: "src/fonts/", to: "../dist/fonts"}
+                    ],
+                }),
+                new ImageminWebpWebpackPlugin({
                     config: [{
-                        test: /\.(jpe?g|png|gif|svg)$/i,
+                        test: /\.(jpe?g|png|gif)$/i,
                         options: {
-                            quality: 75,
+                            quality: 95,
                             lossless: false
                         }
                     }],
-                }
-            )
-        ],
-    })
+                }),
+                new SVGSpritemapPlugin('src/img/svg/*.svg', {
+                    output: {
+                        filename: 'img/sprites/sprite.svg',
+                        svgo: true,
+                        svg4everybody: true,
+                    },
+                    sprite: {
+                        prefix: "",
+                    }
+                })
+            ],
+        },
+    )
     .browserSync({
         server: "./dist/",
         port: 4000,
